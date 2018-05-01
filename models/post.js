7e818d8bba0a49bf0b8d026e002d5744e1ef538e
Vicente Gonzalez <vgonzalez28@gmail.com>
2018-04-30 19:15:19 -0700

    wireframe

--- models/post.js
+++ models/post.js
@@ -1,26 +1,23 @@
 module.exports = function(sequelize, DataTypes) {
   var Post = sequelize.define("Post", {
-  //   NEW Resource POST GOES HERE:::::
-   
-   
-    // title: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    //   validate: {
-    //     len: [1]
-    //   }
-    // },
-    // body: {
-    //   type: DataTypes.TEXT,
-    //   allowNull: false,
-    //   validate: {
-    //     len: [1]
-    //   }
-    // },
-    // category: {
-    //   type: DataTypes.STRING,
-    //   defaultValue: "Personal"
-    // }
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Personal"
+    }
   });
   return Post;
 };
